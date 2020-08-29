76578d5a3ee95df5829156748f5f341aecf1b800
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2020-08-29 13:29:00 +0100

    Better implementation of AsyncTaskLoader with managed Lifecycle

--- app/src/main/java/project/android/headli9es/MainActivity.java
+++ app/src/main/java/project/android/headli9es/MainActivity.java
@@ -1,8 +1,8 @@
 package project.android.headli9es;
 
-import android.app.LoaderManager;
+import androidx.annotation.Nullable;
+import androidx.loader.app.LoaderManager;
 import android.content.Intent;
-import android.content.Loader;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
+import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,7 +25,6 @@ public class MainActivity extends AppCompatActivity implements
 
     private static final int LOADER_ID = 0;
     private String LOG_TAG = MainActivity.class.getName();
-    private LoaderManager loaderManager = getLoaderManager();
     private NewsAdapter newsAdapter;
     static TextView nullNEWS;
     private ProgressBar mNewsProgress;
@@ -37,17 +38,64 @@ public class MainActivity extends AppCompatActivity implements
         mNewsProgress = (ProgressBar) findViewById(R.id.pb_news);
         mNewsRecycler = (RecyclerView) findViewById(R.id.recycler);
 
-        loaderManager.restartLoader(LOADER_ID, null, MainActivity.this);
+        LoaderManager loaderManager = getSupportLoaderManager();
+
+        Bundle seek = new Bundle();
+        seek.putString("link", "https://newsapi.org/v2/top-headlines?country=ng&pageSize=25");
+        loaderManager.initLoader(LOADER_ID, seek,
+                (LoaderManager.LoaderCallbacks) MainActivity.this);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mNewsRecycler.setLayoutManager(layoutManager);
+        mNewsRecycler.setHasFixedSize(true);
+
+        // Create a new newsPopulator that takes a rich (or otherwise empty) list of newsList as input
+        newsAdapter = new NewsAdapter(new ArrayList<News>(), MainActivity.this);
+
+        mNewsRecycler.setAdapter(newsAdapter);
     }
 
     @Override
-    public Loader<List<News>> onCreateLoader (int i, Bundle bundle) {
+    public Loader<List<News>> onCreateLoader (int i, final Bundle bundle) {
         Log.i(LOG_TAG, "onCreateLoader() called");
-        return new NewsLoader(this, "https://newsapi.org/v2/top-headlines?country=ng&pageSize=25");
+        return new AsyncTaskLoader<List<News>>(this) {
+            List<News> result;
+            String address = bundle.getString("link");
+
+            @Override
+            public void deliverResult (List<News> data) {
+                result = data;
+                super.deliverResult(data);
+            }
+
+            @Override
+            protected void onStartLoading () {
+                super.onStartLoading();
+                if (result != null) {
+                    deliverResult(result);
+                } else
+                    forceLoad();
+            }
+
+            @Override
+            public List<News> loadInBackground () {
+                Log.i(LOG_TAG, "This is loadInBackground. I received: " + address);
+
+                    // Don't perform the request if there are no URLs, or the first URL is null.
+                    if (address == null) {
+                        Log.i(this.getClass().getName(), "Conditional check finds null");
+                        return null;
+                    } else {
+                        result = Search.lookUpVolumes(bundle.getString("link"));
+                        Log.i(this.getClass().getName(), "result List data: " + result);
+                        return result;
+                    }
+                }
+        };
     }
 
     @Override
-    public void onLoadFinished (Loader<List<News>> loader, List<News> data) {
+    public void onLoadFinished (androidx.loader.content.Loader<List<News>> loader, List<News> data) {
         // If there is a valid list of {@link News}, then add them to the listPopulator's dataset.
         // This will trigger the RecyclerView to update.
         if (data != null && !data.isEmpty()) {
@@ -56,13 +104,7 @@ public class MainActivity extends AppCompatActivity implements
             mNewsProgress.setVisibility(View.GONE);
             mNewsRecycler.setVisibility(View.VISIBLE);
 
-            LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-            mNewsRecycler.setLayoutManager(layoutManager);
-            mNewsRecycler.setHasFixedSize(true);
-
-            // Create a new newsPopulator that takes a rich (or otherwise empty) list of newsList as input
             newsAdapter = new NewsAdapter(data, MainActivity.this);
-
             mNewsRecycler.setAdapter(newsAdapter);
         } else {
             mNewsProgress.setVisibility(View.VISIBLE);
@@ -71,7 +113,7 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onLoaderReset (Loader loader) {
+    public void onLoaderReset (androidx.loader.content.Loader<List<News>> loader) {
         Log.i(LOG_TAG, "onLoaderReset() called");
         newsAdapter = new NewsAdapter(new ArrayList<News>(), this);
     }
