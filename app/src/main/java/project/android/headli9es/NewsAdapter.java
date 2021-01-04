697678c94191608df3a5f489c708de1f6c106a99
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-01-04 17:38:49 +0100

    Better implementation of AsyncTaskLoader with managed Lifecycle

--- app/src/main/java/project/android/headli9es/NewsAdapter.java
+++ app/src/main/java/project/android/headli9es/NewsAdapter.java
@@ -1,10 +1,13 @@
 package project.android.headli9es;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +15,7 @@ import java.util.List;
 import androidx.recyclerview.widget.RecyclerView;
 import project.android.headli9es.databinding.ForecastBinding;
 
+import static androidx.core.content.ContextCompat.startActivity;
 
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsCarrier> {
 
@@ -21,17 +25,10 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsCarrier> {
     private final ArrayList<News> news;
     protected ForecastBinding binder;
 
-    /**
-     * An interface mechanism to handle news article clicks
-     */
-    protected interface ArticleClickListener {
+    public interface ArticleClickListener {
         void onArticleClickListener (String link);
     }
 
-    /**
-     * @param data     List of News articles
-     * @param listener an implementation of @{link=ArticleClickListener}
-     */
     public NewsAdapter (List<News> data, ArticleClickListener listener) {
         Log.d(LOG_TAG, "This is NewsPopulator.");
         articleClickListener = listener;
@@ -40,42 +37,38 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsCarrier> {
 
     @Override
     public NewsCarrier onCreateViewHolder (ViewGroup parent, int viewType) {
+        Log.d(LOG_TAG, "onCreateViewHolder::");
         Context context = parent.getContext();
         LayoutInflater roller = LayoutInflater.from(context);
 
-        return new NewsCarrier(ForecastBinding.inflate(roller, parent, false));
+        binder = ForecastBinding.inflate(roller, parent, false); //works
+        return new NewsCarrier(binder/*, parent*/);
     }
 
     @Override
     public void onBindViewHolder (NewsCarrier holder, int position) {
+        Log.d(LOG_TAG, "onBindViewHolder.");
         binder.setNews(news.get(position));
         binder.executePendingBindings();
     }
 
     @Override
     public int getItemCount () {
+        Log.d(LOG_TAG,"\narticlesNum::" + news.size());
         return news != null ? news.size() : 0;
     }
 
-
+    /**
+     *
+     */
     protected class NewsCarrier extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        /**
-         * @Link{Constructor} for initializing the Data Binding object,
-         * and setting an @link{View.OnClickListener} on it
-         * @param forecastBinding an auto-gen Data Binding object
-         */
-        public NewsCarrier (ForecastBinding forecastBinding) {
+        public NewsCarrier(ForecastBinding forecastBinding/*, ViewGroup parent*/) {
             super(forecastBinding.getRoot());
             binder = forecastBinding;
             forecastBinding.getRoot().setOnClickListener(this);
         }
 
-        /**
-         * An override of @link{View.OnClickListener}'s @link{onClick} method
-         * for locating the clicked news article
-         * @param view item clicked on
-         */
         @Override
         public void onClick (View view) {
             articleClickListener.onArticleClickListener(news.get(getAdapterPosition()).getPage());
