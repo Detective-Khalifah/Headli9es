a1657798f35c8238e278d45b98ad3563dba9a229
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-05-21 03:05:09 +0100

    Re-factor {@link MainActivity} to use Data Binding. Re-factor setEmptyView() by calling clear() & addAll(Object...) on {@link ArrayAdapter} object.

--- app/src/main/java/project/android/headli9es/MainActivity.java
+++ app/src/main/java/project/android/headli9es/MainActivity.java
@@ -3,54 +3,44 @@ package project.android.headli9es;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.loader.app.LoaderManager;
+import androidx.loader.app.LoaderManager.LoaderCallbacks;
 import androidx.loader.content.AsyncTaskLoader;
 import androidx.loader.content.Loader;
+import project.android.headli9es.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements
-        LoaderManager.LoaderCallbacks<List<News>> {
+        LoaderCallbacks<List<News>> {
 
     private static final int LOADER_ID = 0;
     private String LOG_TAG = MainActivity.class.getName(), section = "home", ap="Vd6bJTsQALVX8fguWnFtpd37xZjch8f5";
     private NewsAdapter newsAdapter;
-    private TextView nullNEWS;
-    private ProgressBar mNewsProgress;
-    protected ListView mNewsList;
+    private ActivityMainBinding mMainBinding;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        nullNEWS = findViewById(R.id.tv_noa);
-        mNewsProgress = findViewById(R.id.pb_news);
-        mNewsList = findViewById(R.id.list_view);
+        mMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         LoaderManager loaderManager = getSupportLoaderManager();
+
+        // Create a new {@link NewsPopulator} that takes an empty, non-null {@link ArrayList} of
+        // {@link News} as input
+        newsAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        mMainBinding.listView.setAdapter(newsAdapter);
+        mMainBinding.listView.setEmptyView(mMainBinding.tvNoa);
 
         Bundle seek = new Bundle();
         seek.putString("link", "https://api.nytimes.com/svc/topstories/v2/" + section + ".json?api-key=" + ap);
         loaderManager.initLoader(LOADER_ID, seek,
                 (LoaderManager.LoaderCallbacks) MainActivity.this);
-
-        // Create a new newsPopulator that takes a rich (or otherwise empty) list of newsList as input
-        newsAdapter = new NewsAdapter(MainActivity.this, new ArrayList<News>());
-
-        mNewsList.setAdapter(newsAdapter);
-    }
-
-    private void setEmptyView () {
-        nullNEWS.setVisibility(View.VISIBLE);
-        mNewsProgress.setVisibility(View.GONE);
-        mNewsList.setVisibility(View.GONE);
     }
 
     @Override
@@ -94,25 +84,24 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onLoadFinished (androidx.loader.content.Loader<List<News>> loader, List<News> data) {
+        mMainBinding.pbNews.setVisibility(View.GONE);
+        newsAdapter.clear();
+
         // If there is a valid list of {@link News}, then add them to the listPopulator's dataset.
         // This will trigger the RecyclerView to update.
         if (data != null && !data.isEmpty()) {
             Log.i(LOG_TAG, "Data not empty in onPostExecute's check");
 
-            mNewsProgress.setVisibility(View.GONE);
-            mNewsList.setVisibility(View.VISIBLE);
-
-            newsAdapter = new NewsAdapter(MainActivity.this, data);
-            mNewsList.setAdapter(newsAdapter);
+            newsAdapter.addAll(data);
         } else {
-            setEmptyView();
+            mMainBinding.tvNoa.setText("0 articles could be fetched!");
         }
     }
 
     @Override
     public void onLoaderReset (androidx.loader.content.Loader<List<News>> loader) {
         Log.i(LOG_TAG, "onLoaderReset() called");
-        newsAdapter = new NewsAdapter(this, new ArrayList<News>());
+        newsAdapter.notifyDataSetInvalidated();
     }
 
 }
