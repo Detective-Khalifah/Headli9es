b3dac512911ef74ba6a02dcbe09daa2f444108ca
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-05-24 04:47:26 +0100

    Make {@link MainActivity} presentable

--- app/src/main/java/project/android/headli9es/MainActivity.java
+++ app/src/main/java/project/android/headli9es/MainActivity.java
@@ -7,7 +7,6 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -33,7 +32,6 @@ public class MainActivity extends AppCompatActivity implements
         LoaderCallbacks<List<News>>, SharedPreferences.OnSharedPreferenceChangeListener {
 
     private static final int LOADER_ID = 0;
-    private static final String LOG_TAG = MainActivity.class.getName();
 
     /** API Base {@link URL}s for the 3 {@link News} outlets. */
     private static final String GUARDIAN_API_BASE_URL = "https://content.guardianapis.com/";
@@ -44,7 +42,7 @@ public class MainActivity extends AppCompatActivity implements
     /** APIs' parameters & paths */
     private static final String GUARDIAN_DEFAULT_PATH = "search";
     private static final String NEWS_DEFAULT_PATH = "top-headlines";
-    private String NY_TIMES_DEFAULT_SECTION = "home.json";
+    private static final String NY_TIMES_DEFAULT_SECTION = "home.json";
 
     /** Authorisation */
     private static final String GUARDIAN_AUTH = "f8981f58-9f90-4bd8-91d7-c5f241f8e433";
@@ -60,8 +58,6 @@ public class MainActivity extends AppCompatActivity implements
 
     private SharedPreferences newsConfig;
 
-    private static LoaderManager loaderManager;
-    
     // Data binding blueprint/class of MainActivity
     private static ActivityMainBinding mMainBinding;
 
@@ -106,7 +102,7 @@ public class MainActivity extends AppCompatActivity implements
         ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = connManager.getActiveNetworkInfo();
         if (netInfo != null && netInfo.isConnected()) {
-            loaderManager = getSupportLoaderManager();
+            LoaderManager loaderManager = getSupportLoaderManager();
             loaderManager.initLoader(LOADER_ID, seek,
                     (LoaderManager.LoaderCallbacks) MainActivity.this);
         } else {
@@ -150,7 +146,6 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public Loader<List<News>> onCreateLoader (int i, final Bundle bundle) {
-        Log.i(LOG_TAG, "onCreateLoader() called");
         return new NewsLoader(this, bundle);
     }
 
@@ -159,15 +154,12 @@ public class MainActivity extends AppCompatActivity implements
         mMainBinding.pbNews.setVisibility(GONE);
         mMainBinding.linArticlesCount.tvNumArticles.setVisibility(View.VISIBLE);
 
-        // TODO: LiveData + ViewModel; Pull-to-Refresh.
         newsAdapter.notifyDataSetChanged();
 
         // If there is a valid list of {@link News}, then add them to the {@link NewsAdapter}'s dataset.
         // This will trigger the {@link ListView} to update.
         if (data != null && !data.isEmpty()) {
-            Log.i(LOG_TAG, "Data not empty in onPostExecute's check");
 
-            // TODO: Find a way to set the two values - articles count & page size- automatically.
             mMainBinding.tvNoa.setVisibility(View.VISIBLE);
             mMainBinding.linArticlesCount.tvNumArticles.setText(getResources().getQuantityString(
                     R.plurals.articles_count,
@@ -184,8 +176,6 @@ public class MainActivity extends AppCompatActivity implements
             else
                 mMainBinding.linArticlesCount.tvPageSize.setVisibility(View.VISIBLE);
 
-            Log.i(LOG_TAG, "Number of articles(List): " + data.get(0).getTotalArticles());
-
             newsAdapter.addAll(data);
         } else {
             mMainBinding.tvNoa.setText(R.string.no_article_fetched);
@@ -195,7 +185,6 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public void onLoaderReset (androidx.loader.content.Loader<List<News>> loader) {
-        Log.i(LOG_TAG, "onLoaderReset() called");
         newsAdapter.clear();
     }
 
@@ -240,10 +229,8 @@ public class MainActivity extends AppCompatActivity implements
 
         final String NY_TIMES_CODE = getString(R.string.ny_times_code);
         final String NEWS_CODE = getString(R.string.news_code);
-        final String GUARDIAN_CODE = getString(R.string.guardian_code);
 
         if (apiCode.equals(NY_TIMES_CODE)) {
-            Log.i(LOG_TAG, "NY_TIMES api selected.");
 
             base = Uri.parse(NY_TIMES_HOST);
             uriBuilder = base.buildUpon();
@@ -255,7 +242,6 @@ public class MainActivity extends AppCompatActivity implements
             seek.putString("code", apiCode);
             seek.putString("link", uriBuilder.toString());
         } else if (apiCode.equals(NEWS_CODE)) {
-            Log.i(LOG_TAG, "newsapi.org api selected.");
 
             base = Uri.parse(NEWS_API_BASE_URL);
             uriBuilder = base.buildUpon();
@@ -271,7 +257,6 @@ public class MainActivity extends AppCompatActivity implements
             seek.putString("code", apiCode);
             seek.putString("link", uriBuilder.toString());
         } else { // GUARDIAN_API_CODE:
-            Log.i(LOG_TAG, "Default api chosen.");
 
             base = Uri.parse(GUARDIAN_API_BASE_URL);
             uriBuilder = base.buildUpon();
@@ -329,17 +314,15 @@ public class MainActivity extends AppCompatActivity implements
             if (id == mMainBinding.btnNewsApi.getId()) {
                 selectedAPI = getString(R.string.news);
                 newsConfig.edit().putString(NEWS_OUTLET_PREFERENCE_KEY, getString(R.string.news_code)).apply();
-                onSharedPreferenceChanged(newsConfig, NEWS_OUTLET_PREFERENCE_KEY);
             } else if (id == mMainBinding.btnNyTimes.getId()) {
                 selectedAPI = getString(R.string.ny_times);
                 newsConfig.edit().putString(NEWS_OUTLET_PREFERENCE_KEY, getString(R.string.ny_times_code)).apply();
-                onSharedPreferenceChanged(newsConfig, NEWS_OUTLET_PREFERENCE_KEY);
             } else {
                 selectedAPI = getString(R.string.guardian);
                 newsConfig.edit().putString(NEWS_OUTLET_PREFERENCE_KEY, getString(R.string.guardian_code)).apply();
-                onSharedPreferenceChanged(newsConfig, NEWS_OUTLET_PREFERENCE_KEY);
             }
 
+            onSharedPreferenceChanged(newsConfig, NEWS_OUTLET_PREFERENCE_KEY);
             Snackbar.make(MainActivity.this, (View) mMainBinding.frameSnack,
                     selectedAPI + " selected", Snackbar.LENGTH_LONG).show();
         }
